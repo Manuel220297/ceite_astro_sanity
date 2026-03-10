bffd86d6054e41349fc6e9840f7e09155687693a
Manuel <1-220297@asiatech.edu.ph>
2026-03-10 16:45:36 +0800

    sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+import sanity from '@sanity/astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [],
+    },
+  },
+
+  integrations: [
+    sanity({
+      projectId: 'yiulggd9',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: '2026-03-10', // insert the current date to access the latest version of the API
+    }),
+    react(),
+  ],
+});
---
+++ news/first-news-local.md
@@ -0,0 +1,9 @@
+---
+title: First News
+pubDate: 2026-03-10T11:18:00.000+08:00
+image: 
+slug: first-news
+---
+Ackemi
+## H2 Testing
+AAA
