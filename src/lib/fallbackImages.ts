3d302e345d56c4747fcd8bc2f339e897f925a850
Manuel220297 <1-220297@asiatech.edu.ph>
2026-05-20 00:22:43 +0800

    curriculum design 2

--- src/lib/fallbackImages.ts
+++ src/lib/fallbackImages.ts
@@ -2,6 +2,7 @@ export const DEFAULT_IMAGE = {
   KEYBOARD: '/images/default_keyboard.avif',
   MOUSE: '/images/default_mouse.jpg',
   AVATAR: '/images/default_avatar.png',
+  AVATAR2: '/images/default_avatar.svg',
   BACKGROUND: '/images/default_background.avif',
   LOGO: '/images/asiatech_logo.avif',
 } as const;
